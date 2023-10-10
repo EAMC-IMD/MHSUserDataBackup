341d116227bf865531fa4b8e5996806dcd5920ab
sapph42 <nick.gibson@gmail.com>
2023-10-10 19:16:12 -0400

    Update for project links

--- ProfileData.cs
+++ ProfileData.cs
@@ -2,11 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using ChromiumBookmarkManager;
 using System.Text.RegularExpressions;
 
